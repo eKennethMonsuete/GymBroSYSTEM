de450e087121c09a0e8d7af0e2d18221a1546253
ivolimajr <ivo.sl.jr@gmail.com>
2024-09-24 19:01:03 -0300

    Geração do token

--- GymBroAPI/Program.cs
+++ GymBroAPI/Program.cs
@@ -1,10 +1,17 @@
 using GymBroINFRA.Context;
+using GymBroINFRA.Models;
 using GymBroINFRA.Repository;
 using GymBroSERVICE.MeasuresService;
 using GymBroSERVICE.PersonalService;
 using GymBroSERVICE.StudentService;
 using GymBroSERVICE.UserServices;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Configuration;
+using System.Text;
 
 
 
@@ -15,6 +22,39 @@ internal class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
+
+        var tokenConfigurations = new TokenConfiguration();
+
+        new ConfigureFromConfigurationOptions<TokenConfiguration>(
+                builder.Configuration.GetSection("TokenConfiguration")
+            )
+            .Configure(tokenConfigurations);
+
+        builder.Services.AddSingleton(tokenConfigurations);
+
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = tokenConfigurations.Issuer,
+                ValidAudience = tokenConfigurations.Audience,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenConfigurations.Secret))
+            };
+        });
+
+        builder.Services.AddAuthorization(auth =>
+        {
+            auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder().AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme).RequireAuthenticatedUser().Build());
+        });
+
 
         builder.Services.AddSwaggerGen();
         builder.Services.AddControllers();
--- GymBroSERVICE/UserServices/UserServices.cs
+++ GymBroSERVICE/UserServices/UserServices.cs
@@ -1,6 +1,12 @@
 ﻿using GymBroINFRA.Entity;
+using GymBroINFRA.Models;
 using GymBroINFRA.Repository;
 using GymBroSERVICE.UserServices.DTO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace GymBroSERVICE.UserServices
 {
@@ -8,10 +14,12 @@ namespace GymBroSERVICE.UserServices
     {
 
         private readonly IRepository<User> _repository;
+        private readonly TokenConfiguration _configuration;
 
-        public UserServices(IRepository<User> repository)
+        public UserServices(IRepository<User> repository, TokenConfiguration tokenConfiguration)
         {
             _repository = repository;
+            _configuration = tokenConfiguration;
         }
 
         public LoginResultDTO Login(LoginDTO authDTO)
@@ -20,7 +28,6 @@ namespace GymBroSERVICE.UserServices
 
             if (user == null)
                 throw new Exception("Credenciais inválidas");
-                
 
             bool match = BCrypt.Net.BCrypt.EnhancedVerify(authDTO.Password, user.Password);
 
@@ -28,14 +35,53 @@ namespace GymBroSERVICE.UserServices
                 throw new Exception("Credenciais inválidas");
 
             //GERAR O TOKEN
-
+            string token = GenerateToken(user);
 
 
             return new LoginResultDTO
             {
-                AccessToken = "",
-                ExpiratedAt = DateTime.Now,
+                AccessToken = token,
+                ExpiratedAt = DateTime.Now.AddMinutes(_configuration.Minutes)
             };
+        }
+
+
+        private string GenerateToken(User user)
+        {
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, user.Email)
+            };
+
+            if (user.StudentId != null)
+            {
+
+                claims.Add(new Claim(ClaimTypes.Sid, user.StudentId.ToString()));
+                claims.Add(new Claim(ClaimTypes.Role, "STUDENT"));
+            }
+            else
+            {
+                claims.Add(new Claim(ClaimTypes.Sid, user.PersonalId.ToString()));
+                claims.Add(new Claim(ClaimTypes.Role, "PERSONAL"));
+            }
+
+            return GetToken(claims);
+        }
+
+        private string GetToken(IEnumerable<Claim> claims)
+        {
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.Secret));
+            var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+            var options = new JwtSecurityToken(issuer: _configuration.Issuer,
+                    audience: _configuration.Audience,
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(_configuration.Minutes),
+                    signingCredentials: signinCredentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(options);
         }
     }
 }
